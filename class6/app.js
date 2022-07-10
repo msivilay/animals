b54c82a53ce5652edb901506923d6683e3f4003e
Marina Sivilay <msivilay@gmail.com>
2022-07-10 05:35:15 -0700

    added variable faveanimal and readded hr

--- class6/app.js
+++ class6/app.js
@@ -1,15 +1,18 @@
 'use strict'
 
 let yourname = prompt('Hi, what is your name?');
-console.log('Hello, ' + yourname);
+console.log('Welcome, ' + yourname);
+
+let faveanimal = prompt ("What is your favorite animal?");
+console.log(faveanimal + "... cool.");
 
 let age = prompt('How old are you?');
 let message;
 
 if(age <= 12){
-    message = 'Hey there, kiddo!';
+    message = "It's sweet being a kid!";
 } else if (age <= 18){
-    message = 'Hey there, teen!';
+    message = "Cherish your teen years!";
 } else if(age <= 21){
     message = "Sorry, I can't get you a beer ;P";
 } else {
