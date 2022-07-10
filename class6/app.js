9d3745330824f4a45edd2bedb43da2f21b227325
Marina Sivilay <msivilay@gmail.com>
2022-07-10 05:10:11 -0700

    adding Javascript prompts

---
+++ class6/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let yourname = prompt('Hi, what is your name?');
+console.log('Hello, ' + yourname);
+
+let age = prompt('How old are you?');
+let message;
+
+if(age <= 12){
+    message = 'Hey there, kiddo!';
+} else if (age <= 18){
+    message = 'Hey there, teen!';
+} else if(age <= 21){
+    message = "Sorry, I can't get you a beer ;P";
+} else {
+    message = "Being an adult isn't all it's cracked up to be, huh?";
+}
+
+document.write('Hello ' + username + '! ' + message);
