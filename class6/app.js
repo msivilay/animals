8ee7dc1226cf6683b4a12207287207472e06d086
Marina Sivilay <msivilay@gmail.com>
2022-07-10 05:49:44 -0700

    changed or fixed quote marks

--- class6/app.js
+++ class6/app.js
@@ -3,18 +3,18 @@
 let yourname = prompt('Hi, what is your name?');
 console.log('Welcome, ' + yourname);
 
-let faveanimal = prompt ("What is your favorite animal?");
-console.log(faveanimal + "... cool.");
+let faveanimal = prompt ('What is your favorite animal?');
+console.log(faveanimal + '... cool.');
 
 let age = prompt('How old are you?');
 let message;
 
 if(age <= 12){
-    message = "It's sweet being a kid!";
+    message = "It's sweet being a kid! Enjoy learning about animals.";
 } else if (age <= 18){
     message = "Cherish your teen years!";
-} else if(age < 21){
-    message = "Sorry, I can't get you a beer ;P";
+} else if(age <= 21){
+    message = "Can I see your ID? ;P";
 } else {
     message = "Being an adult isn't all it's cracked up to be, huh? Enjoy reading about some animals.";
 }
